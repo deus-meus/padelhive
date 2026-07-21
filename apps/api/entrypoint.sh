70bd65f69f6275af8b46b46ceba530f6f3caa3ce
Ubuntu <ubuntu@localhost.localdomain>
2026-07-21 11:21:18 +0800

    fix(api): compile seed.ts using tsc inside entrypoint before execution

--- apps/api/entrypoint.sh
+++ apps/api/entrypoint.sh
@@ -5,7 +5,9 @@ echo "Running Prisma Migrations..."
 npx prisma migrate deploy
 
 echo "Running Prisma Seeder..."
-npm run prisma:seed
+# Compile seeder to JS and run it using pure node
+npx tsc prisma/seed.ts
+node prisma/seed.js
 
 echo "Starting API Server..."
 npm run start
